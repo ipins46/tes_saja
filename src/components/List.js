ba5f2bc58b226966caede80eadb59553647acdd0
m.arifin46 <muhamadarifin428@gmail.com>
2021-02-08 01:02:33 +0700

    tes doang nih
    iya ini tes doang kok

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Paper} from "@material-ui/core";
+export default function List() {
+  return (
+    <div>
+      <Paper>
+    
+      </Paper>
+    </div>
+  );
+}
